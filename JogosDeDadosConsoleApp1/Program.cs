99dad34a7a4c9dcaa5293378fefe152c31fd02b2
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-20 10:13:35 -0300

    Implementada o avanç ode posições do jogador

--- JogosDeDadosConsoleApp1/Program.cs
+++ JogosDeDadosConsoleApp1/Program.cs
@@ -1,28 +1,52 @@
-﻿namespace JogosDeDadosConsoleApp1;
+﻿using System.ComponentModel.Design;
+
+namespace JogosDeDadosConsoleApp1;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        const int limiteLinhaDeChegada = 30;
 
         while (true)
         {
-            Console.Clear();
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("***Jogos de Dados***");
-            Console.WriteLine("-----------------------");
+            int posicaoDoJogador = 0;
+            bool jogoEmAndamento = true;
+            while (jogoEmAndamento)
+            {
+                Console.Clear();
+                Console.WriteLine("-----------------------");
+                Console.WriteLine("***Jogos de Dados***");
+                Console.WriteLine("-----------------------");
 
-            Console.Write("Pressione ENTER para lançar  dado ...");
-            Console.ReadLine();
+                Console.Write("Pressione ENTER para lançar  dado ...");
+                Console.ReadLine();
 
-            Random geradorDeNumeros = new Random();
-            int resultado = geradorDeNumeros.Next(1,7);
+                Random geradorDeNumeros = new Random();
+                int resultado = geradorDeNumeros.Next(1, 7);
 
-            Console.WriteLine($"O valor sorteado foi: [{resultado}] ");
+                Console.WriteLine($"O valor sorteado foi: [{resultado}] ");
+                posicaoDoJogador += resultado;
+
+                if (posicaoDoJogador >= limiteLinhaDeChegada)
+                {
+                    jogoEmAndamento = false;
+                    Console.WriteLine("Parabéns você alcançou a linha de chegada!");
+                }
+                else
+                    Console.WriteLine($"Você está na posição: {posicaoDoJogador} de {limiteLinhaDeChegada}!");
+
+                Console.ReadLine();
+
+
+
+
+
+
+            }
 
             Console.WriteLine("Deseja continuar? S/N ");
             string opcaocontinuar = Console.ReadLine()!.ToUpper();
-
 
             if (opcaocontinuar != "S") ;
             break;
