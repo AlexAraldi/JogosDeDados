1b800f2f41667b089262ccc8a5a42aa2b7e97246
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-20 11:40:07 -0300

    Correção de bug e adiçao do bônus

--- JogosDeDadosConsoleApp1/Program.cs
+++ JogosDeDadosConsoleApp1/Program.cs
@@ -66,7 +66,7 @@ internal class Program
             Console.WriteLine("Deseja continuar? S/N ");
             string opcaocontinuar = Console.ReadLine()!.ToUpper();
 
-            if (opcaocontinuar != "S") ;
+            if (opcaocontinuar != "S") 
             break;
             
         }
